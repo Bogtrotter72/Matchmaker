3c881dd631766fabfbc1a3b2cb7b31a8aaa18e8d
Ciaran McCloskey <2ciaran7@gmail.com>
2020-02-08 19:06:51 +0000

    begin rewrite of code in JavaScript

--- main.js
+++ main.js
@@ -1,355 +1,230 @@
-(function($) {
+class AudioController {
+    constructor() {
+        this.successAudio = new Audio('Success.mp3');
+        this.successAudio.volume = 0.5;
+        this.failAudio = new Audio('Failure.mp3');
+        this.failAudio.volume = 0.3;
+        this.winAudio = new Audio('Game-win.mp3');
+        this.winAudio.volume = 0.5;
+        this.flipAudio = new Audio('card-flip.mp3');
+        this.flipAudio.volume = 0.5;
+    }
+    cardFlip() {
+        this.flipAudio.play();
+    }
+    cardMatch() {
+        this.successAudio.play();
+    }
+    cardNoMatch() {
+        this.failAudio.play();
+    }
+    gameWin() {
+        this.winAudio.play();
+    }
+}
 
-    let bonus = 0;
-    let cardFlipped = false;
-    let cardClick = 0;
-    let difficultyLevel = 0;
-    let firstCard, secondCard, firstCardImg, secondCardImg, cardsRemaining;
-    let gameWonCond = false;
-    let lockBoard = false;
-    let score = 0;
-    let timer, totalTime, timeOnTheClock;
-    let successAudio = new Audio('Success.mp3');
-    successAudio.volume = 0.5;
-    let failAudio = new Audio('Failure.mp3');
-    failAudio.volume = 0.3;
-    let winAudio = new Audio('Game-win.mp3');
-    winAudio.volume = 0.5;
-    let flipAudio = new Audio('card-flip.mp3');
-    flipAudio.volume = 0.5;
-    let beginnerImgs = ['_001', '_002', '_003','_004', '_005', '_006'];
-
-
-
-    const imageArray = [
-        'https://res.cloudinary.com/bogtrotter72/image/upload/v1581008483/Milestone%202/Final%20Images/Beginner_001_bhay28.png',
-        'https://res.cloudinary.com/bogtrotter72/image/upload/v1581008484/Milestone%202/Final%20Images/Beginner_001__match_eyrmdw.png',
-        'https://res.cloudinary.com/bogtrotter72/image/upload/v1581008482/Milestone%202/Final%20Images/Beginner_002_xlg3f7.png',
-        'https://res.cloudinary.com/bogtrotter72/image/upload/v1581008481/Milestone%202/Final%20Images/Beginner_002__match_f57myd.png',
-        'https://res.cloudinary.com/bogtrotter72/image/upload/v1581008482/Milestone%202/Final%20Images/Beginner_003_o1jrd7.png',
-        'https://res.cloudinary.com/bogtrotter72/image/upload/v1581008480/Milestone%202/Final%20Images/Beginner_003__match_lzysh0.png',
-        'https://res.cloudinary.com/bogtrotter72/image/upload/v1581008482/Milestone%202/Final%20Images/Beginner_004_rmtvyq.png',
-        'https://res.cloudinary.com/bogtrotter72/image/upload/v1581008481/Milestone%202/Final%20Images/Beginner_004__match_lp5lya.png',
-        'https://res.cloudinary.com/bogtrotter72/image/upload/v1581008483/Milestone%202/Final%20Images/Beginner_005_jkkwpo.png',
-        'https://res.cloudinary.com/bogtrotter72/image/upload/v1581008483/Milestone%202/Final%20Images/Beginner_005__match_ozvn4w.png',
-        'https://res.cloudinary.com/bogtrotter72/image/upload/v1581008481/Milestone%202/Final%20Images/Beginner_006_wimc5m.png',
-        'https://res.cloudinary.com/bogtrotter72/image/upload/v1581008482/Milestone%202/Final%20Images/Beginner_006__match_vrisnb.png'
-    ];
-
-    // Initialize the game
-    gameInit();
-
-    // Call the game setup function
-    gameSetup();
-
-
-    // Reveal the game board and allow game play
-    $('.btn-startGame').click(function() {
-        $('.page-title').fadeOut('slow');
-        $('.hud').css('display', 'flex');
-        $('.hud').fadeIn('slow');
-
-        $('#main-page__bg-img').css('animation', 'spinOut 1198ms ease-in forwards');
-        $('.bg-img__wrapper').fadeOut(1198);
-        $('.btn-wrapper__game-start').fadeOut(1198);
-
-        // Delay calling the timer until the spinOut animation is complete
-        setTimeout(function() {
-            gameCounter(0);
-        }, 1198);
-    })
-
-
-
-    // Rotate main menu buttons out and level select buttons in
-    $('.btn-selectLevel').click(function() {
-        selectLevelBtnAnim();
-    })
-    
-    // Fade out the level select buttons and fade in game info
-    $('.btn__level-select').click(function() {
-        $('.btn__level-select').fadeOut('slow');
-        $('.btn-wrapper__level-select').fadeOut('slow');
-        $('.game-info-container').fadeIn(3000);
-    });
-
-    // Set the timer value based on the level selected
-    $('.btn-beginner').click(function() {
-        difficultyLevel = 1;
-    });
-    $('.btn-intermediate').click(function() {
-        difficultyLevel = 3;
-    });
-    $('.btn-advanced').click(function() {
-
-        difficultyLevel = 7;
-    });
-
-    // Fade out game info, fade in the game board and set the timer
-    // $('.btn__game-start').click(function() {
-    //     $('.game-info-container').fadeOut('slow');
-    //     $('.flip-card-game-container').fadeIn(3000).css('display', 'flex');
-    //     $('.game-timer').fadeIn(3000);
+class PsychoMatch{
+    constructor(cards) {
+        this.cardsArray = cards;
         
-    // });    
+        this.beginnerImgs = ['_001', '_002', '_003', '_004', '_005', '_006'];
+        this.audioController = new AudioController();
+    }
+    startGame() {
+        this.bonus = 0;
+        this.cardClicks = 0;
+        this.cardFlipped = false;
+        this.cardsRemaining = this.cardsArray.length;
+        this.firstCard = null;
+        this.lockBoard = false;
+        this.score = 0;
+        this.secondCard = null;
+        this.startTime = 0;
+        setTimeout( ()=> {
+            this.shuffleCards();
+            this.counter = this.gameCounter(this.startTime);
+        }, 500);
+        this.resetCards();
+    }
+    resetCards() {
+        this.cardsArray.forEach(card => {
+            card.classList.remove("flipped");
+            card.style.animation = "none";
+        })
+    }
 
-    $('.btn-playAgain__true').click(resetGame);
+    gameCounter(time) {
+        return setInterval( () => {
+            time++;
+            if(time === 5) this.gameOver();
+            $('.game-timer').html('Time: ' + time);
+            this.totalTime = time;
+        }, 1000);
 
+    }
+    flipCard(card) {
+        if(this.lockBoard) return;
 
-    /* GAME FUNCTIONALITY */
+        if(card === this.firstCard) return;
 
-    function gameInit() {
-        $('#main-page__bg-img').animate({
-            transform: 'scale(1)'
-        }, 3000);
-    };
+        card.classList.add("flipped");
+        this.audioController.cardFlip();
 
-    function gameSetup() {
-
-        // Set the card back images
-        $('.card__back-img').each(function() {
-            $(this).attr('src', 'https://res.cloudinary.com/bogtrotter72/image/upload/v1580943175/Milestone%202/Final%20Images/Card-Back__Mob_003_us1l0q.png');
-        });
-
-        // Set the cards remaining value
-        cardsRemaining = imageArray.length;
+        this.cardClicks++;
         
-        // Shuffle the card face images
-        shuffleCards(imageArray);
-
-        // Set the initial game play conditions
-        cardFlipped = false;
-
-        $('.card').click(playGame);
-    };
-
-    function selectLevelBtnAnim() {
-        $('.btn-selectLevel').css('transform', 'translateX(-100px) rotateY(-90deg)').delay(1300).fadeOut();
-            
-            $('.btn-credits').css({
-                'transform': 'translateX(-100px) rotateY(-90deg)',
-                'transition-delay': '0.5s'
-            }).delay(1300).fadeOut();
-            
-            $('.btn-quit').css({
-                'transform': 'translateX(-100px) rotateY(-90deg)',
-                'transition-delay': '1.0s'
-            }).delay(1300).fadeOut();
-    
-            $('.btn-wrapper__level-select').fadeIn();
-    
-            $('.btn-beginner').fadeIn('slow').css({
-                'transform': 'rotateY(0deg)',
-                'opacity': 1,
-                'transition-delay': '1.5s'
-            });
-    
-            $('.btn-intermediate').fadeIn('slow').css({
-                'transform': 'rotateY(0deg)',
-                'opacity': 1,
-                'transition-delay': '2s'
-            });
-    
-            $('.btn-advanced').fadeIn('slow').css({
-                'transform': 'rotateY(0deg)',
-                'opacity': 1,
-                'transition-delay': '2.5s'
-            });
-    
-    };
-
-    function playGame() {
-        // Prevent clicking if two, non-matching cards are already flipped
-        if(lockBoard) return;
-
-        // Prevent double clicks
-        if(this === firstCard) return;
-
-        flipAudio.play();
-        $('.game-container').css('animation', 'none');
-
-
-        // Flip the card
-        $(this).addClass('flipped');
-        cardClick += 1;
-
-        // Register the first flipped card
-        if(!cardFlipped) {
-            cardFlipped = true;
-            firstCard = this;
-            firstCardImg = $(this).find(">:first-child").find(">:first-child").attr('src');
-        } else {
-            // Register the second flipped card
-            cardFlipped = false;
-            secondCard = this;
-            secondCardImg = $(this).find(">:first-child").find(">:first-child").attr('src');
-
-
-            function checkForMatch() {
-                for(let i = 0; i < beginnerImgs.length; i++) {
-                    if( firstCardImg.includes('Beginner' + beginnerImgs[i]) && secondCardImg.includes('Beginner' + beginnerImgs[i]) ) {
-                        return true;
-                    }
-                }
-            };  
-
-
-            // Do the flipped cards match
-            if( checkForMatch() ) {
-
-                $(firstCard).css('animation', 'match 500ms ease-in-out');
-                $(secondCard).delay(250).css('animation', 'match 1000ms ease-in-out');
-
-
-                cardsRemaining -=2;
-
-                if (cardsRemaining === 0) {
-                    gameWonCond = true;
-                    setTimeout(() => {
-                        winAudio.play()
-                    }, 500);
-                    gameWon();
-                } else {
-                    setTimeout(() => {
-                        successAudio.play()
-                    }, 500);
-                };
-                $(firstCard).off('click');
-                $(secondCard).off('click');
-
+    }
+    gameOver() {
+        clearInterval(this.counter);
+        this.gameWonModal = document.getElementById("game-won");
+        this.fadeIn = 0;
+        this.time = setInterval( () => {
+            if(this.fadeIn === 10) {
+                clearInterval(this.time);
             } else {
-                // If two, non-matching cards have been flipped lock the board
-                lockBoard = true;
-
-                // Flip the cards back after 1.5 second delay and release the board
-                setTimeout(function() {
-                    $('.game-container').css('animation', 'noMatch 250ms ease-in-out');
-
-
-                    failAudio.play()
-                    $(firstCard).removeClass('flipped');
-                    $(secondCard).removeClass('flipped');
-                    firstCard= null;
-                    secondCard= null;
-                    lockBoard = false;
-                }, 1000);
-
+                this.fadeIn += 1;
+                this.gameWonModal.style.opacity = this.fadeIn/10;
+                this.gameWonModal.style.display = "flex";
             }
-        };
+        }, 120);
 
-    };
-
-    
-    // Fisher-Yates shuffle algorithm:
-    function shuffleCards(imageArray) {
-    
-        let m = imageArray.length;
-        let t, i;
-    
-        // While there are elements left to shuffle
-        while(m) {
-            // Pick a random element
-            i = Math.floor(Math.random() * m--);
-    
-            // And swap it with the current element
-            t = imageArray[m];
-            imageArray[m] = imageArray[i];
-            imageArray[i] = t;
+    }
+    shuffleCards() {
+        for (let i = this.cardsArray.length-1; i > 0;  i--) {
+            let randIndex = Math.floor(Math.random() * (i+1));
+            this.cardsArray[randIndex].style.order = 1;
+            this.cardsArray[i].style.order = randIndex;
         }
+    }
+}
+
+
+function gameInit() {
+
+    let cards = Array.from(document.getElementsByClassName('card'));
+    cardsRemaining = cards.length;
+
+    let game = new PsychoMatch(cards);
     
-        // Place the images on the card front faces
-        for(i = 0; i < imageArray.length; i++) {
-            cardImage = imageArray[i];
-            $('.card__front-img').eq(i).attr('src', cardImage);
-        }
-     };
     
+    document.getElementById("btn-startGame").addEventListener('click', () => {
+        let startBtn = document.getElementById("btn-startGame");
+        let startBtnWrap = document.getElementById("btn-wrapper__game-start");
+        let pageTitle = document.getElementById("page-title");
+        let bgImgWrap = document.getElementById("bg-img__wrapper");
 
-     /* TIMER FUNCTIONALITY */
-     function gameCounter(timeOnTheClock) {
 
-
-        // Delay timer start until animation end
-        timer = setInterval(gameTimer, 1000);
-
-         function gameTimer () {
-            if(gameWonCond) stopTimer();
-            timeOnTheClock++;
-            $('.game-timer').html('Time: ' + timeOnTheClock);
-            totalTime = timeOnTheClock;
+        let fadeOut = 10;
+    
+        let time = setInterval( () => {
+            if(fadeOut === 0) {
+                clearInterval(time);
+            } else {
+                fadeOut -= 1;
+                startBtn.style.opacity = fadeOut/10;
+                startBtnWrap.style.opacity = fadeOut/10;
+                pageTitle.style.opacity = fadeOut/10;
+                bgImgWrap.style.opacity = fadeOut/10;
+                if (pageTitle.style.opacity === "0") {
+                    pageTitle.style.display = "none";
+                    startBtnWrap.style.display = "none";
+                    bgImgWrap.style.display = "none";
+                };
             }
-        };
-
-    function stopTimer() {
-        clearInterval(timer);
-    };
+        }, 120);
     
-    // Game win and reset
-    function gameWon() {
+        let hudDisplay = document.getElementById("hud");
+        let fadeIn = 0;
+        let time2 = setInterval( () => {
+            if(fadeIn === 10) {
+                clearInterval(time2);
+            } else {
+                fadeIn += 1;
+                hudDisplay.style.opacity = fadeIn/10;
+                hudDisplay.style.display = "flex";
+            }
+        }, 120);
+    
+        document.getElementById("main-page__bg-img").style.animation = "spinOut 1198ms ease-in forwards";
+        game.startGame();
 
-        $.each($('.card'), function() {
-            $(this).delay(250).css('animation', 'spinInSpinOut 2396ms ease-in forwards');
-            $(this).removeClass('flipped');
-        });
+    });
+    
+    cards.forEach(card => {
+        card.addEventListener('click', () => {
+            game.flipCard(card);            
+        })
+    });
 
-        $('.game-won').fadeIn(500);
-        $('.btn-wrapper__game-won').fadeIn(500);
+};
 
-        /* REPLACE THIS WITH A PLAY AGAIN BUTTON */
-        $('.btn-wrapper__game-start').fadeIn(1198);
-        resetGame();
-
-
-        /* TO BE COMPLETED */
-        // New method of calculating bonus required!!
-        // bonus = ( (timeRemaining - timeOnTheClock) * difficultyLevel ) * 10;
-
-        score = Math.floor( (imageArray.length / cardClick) * imageArray.length * 100 + bonus );
-        $('.game-won__score').html('Score: ' + score);
-
-        console.log(cardClick);
-        console.log(totalTime);
-        // console.log(bonus);
-        gameWonCond = true;
-    };
+document.addEventListener("DOMContentLoaded", gameInit());
 
 
-    function resetGame() {
-        $('.card').removeClass('flipped');
-        $('.game-won').fadeOut(500);
-        $('.btn-wrapper__game-won').fadeOut(500);
-        lockBoard = false;
-        gameWonCond = false;
-        cardClick = 0;
-        firstCard= null;
-        secondCard= null;
-        $('.game-info > p:eq(2)').remove();
 
 
-        $('.flip-card-game-container').css('display', 'none');
-        $('.game-timer').css('display', 'none');
 
-        $('.btn-wrapper__level-select').fadeIn();
-        $('.btn__level-select').css('transform', 'translateX(150px) rotateY(90deg)');
 
-        $('.btn-beginner').fadeIn('slow').css({
-            'transform': 'rotateY(0deg)',
-            'opacity': 1,
-            'transition-delay': '0.5s'
-        });
 
-        $('.btn-intermediate').fadeIn('slow').css({
-            'transform': 'rotateY(0deg)',
-            'opacity': 1,
-            'transition-delay': '1s'
-        });
 
-        $('.btn-advanced').fadeIn('slow').css({
-            'transform': 'rotateY(0deg)',
-            'opacity': 1,
-            'transition-delay': '1.5s'
-        });
 
-        gameSetup();
-    };
-}(jQuery));
+
+
+
+
+
+
+
+// if(!this.cardFlipped) {
+//     this.cardFlipped = true;
+//     this.firstCard = card;
+//     this.firstCardSrc = this.firstCard.firstElementChild.firstElementChild.src;
+// } else {
+//     this.cardFlipped = false;
+//     this.secondCard = card;
+//     this.secondCardSrc = this.secondCard.firstElementChild.firstElementChild.src;
+
+
+//     function checkForMatch() {
+//         for (let i = 0; i < this.beginnerImgs.length; i++) {
+//             if (this.firstCardSrc.includes('Beginner' + this.beginnerImgs[i]) && this.secondCardSrc.includes('Beginner' + this.beginnerImgs[i])) {
+//                 console.log('true');
+//                 return true;
+//             }
+//         }
+//     };
+
+//     if(checkForMatch) {
+//         this.firstCard.style.animation = "match 500ms ease-in-out";
+//         this.secondCard.style.animation = "match 500ms ease-in-out";
+        
+//         cardsRemaining -= 2;
+
+//         if (cardsRemaining === 0) {
+//             gameWonCond = true;
+//             setTimeout(() => {
+//                 this.audioController.gameWin();
+//             }, 500);
+//             gameWon();
+//         } else {
+//             setTimeout(() => {
+//                 this.audioController.cardMatch();
+//             }, 500);
+//         };
+//         this.firstCard.removeEventListener("click", cardCheck(), false);
+//         this.secondCard.removeEventListener("click", cardCheck(), false);
+//     } else {
+//         this.lockBoard = true;
+
+//         setTimeout(() => {
+//             this.gameContainer = document.getElementById();
+//             this.gameContainer.style.animation = "noMatch 250ms ease-in-out";
+//             this.audioController.noMatch();
+
+
+//             this.firstCard.removeClass('flipped');
+//             this.secondCard.removeClass('flipped');
+//             this.lockBoard = false;
+//         }, 1000);
+//     };
+    
+// }
